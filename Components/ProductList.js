1470efcf2ad23ab71241fd30847b9fe46dfed61f
Camillebfs <beaufilscamille@yahoo.fr>
2022-11-28 11:54:48 +0100

    modif article

--- Components/ProductList.js
+++ Components/ProductList.js
@@ -5,11 +5,14 @@ const  ProductList = {
 		  { 
 			id: 0, 
 			name: "The Crown saison 5: retour sur la décennie infernale des Windsor",
-			router:"/products", 
+			router:"/productDesc", 
 			Corps:'Les années 1990, marquées par de nombreux changements politiques, ont vu la famille royale se déliter sur le plan privé. Rien de tel qu un exhaustif rappel des faits pour se préparer au visionnage de cette nouvelle saison.La cinquième saison de The Crown, la série britannique consacrée а la vie d Elizabeth II d Angleterre, est sortie le 9 novembre. Le nouveau volet couvre les événements des années 1990 qui sur le plan politique et privé ont défini cette période de son règne. Mais que s est-il passé exactement pendant ces années?', 
 			
 		},
-		  { id: 1, name: "La “Mercredi” de la famille Addams ressuscitée par Tim Burton",routeProd:"/products/1" }
+		  { id: 1, 
+			name: "La “Mercredi” de la famille Addams ressuscitée par Tim Burton",
+			router:"/productDesc" 
+		}
 		]
 	  }
 	}, 
