1580b584bd7cf72ca7e256e7e05a3078ef089cb4
SSEThan <ethan.desvages@my-digital-school.org>
2022-11-28 10:54:22 +0100

    Refont et ajout du formulaire

--- Components/FormFils.js
+++ Components/FormFils.js
@@ -1,18 +1,43 @@
 const FormFils={
     template:
-       `<div class="form-creation>
+       `
+       <!--Formulaire Création Article--!>
+       <div class="form-creation>
        <form @submit.prevent="onSubmit">
         <div>
-        <label for="name">Nom de l'article</label>
-        <input id="name" v-model="name" placeholder="Nom de l'article"></input>
+            <label for="name">Nom de l'article</label>
+                <input id="name" v-model="name" placeholder="Nom de l'article"></input>
+            
+            <br><br>
+            <label for="categorie">La catégorie :</label>
+                <select name="categorie" id="categorie">
+                    <option value="">Categories :</option>
+                    <option value="serie">Série</option>
+                    <option value="film">Film</option>
+                    <option value="anime">Animé</option>
+            </select>
 
-        <label for="categorie">La catégorie</label>
-        <input id="categorie" v-model="name" placeholder="La catégorie"></input>
+            <br><br>
+            <label for="texte-article">Texte de l'article</label>
+                <textarea id="textexte-articlete" name="Texte de l'article"></textarea>
 
-        <label for="nomauteur">Le nom de l'auteur</label>
-        <input id="nomauteur" v-model="nomauteur" placeholder="Nom de l'auteur"></input>
-        </form>
+                <br><br>
+            <label for="nomauteur">Le nom de l'auteur</label>
+                <input id="nomauteur" v-model="nomauteur" placeholder="Nom de l'auteur"></input>
+            
+                <br><br>
+                <label for="url">URL de la source</label>
+                <input type="url" name="url" id="url" placeholder="https://example.com" pattern="https://.*" size="30" required>
+
+                <br><br>
+            <label for="illustration">Image d'illustratoion:</label>
+                <input type="file" id="illustration" name="illustration" accept="image/png, image/jpeg">
+            
+                </form>
         </div class="form-creation>
+
+            
+
     `,
     data() {
         return {
